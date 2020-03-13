c9b3fb8727ccbcf0eeee098f6fa7b98ffba056c8
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-13 17:00:49 +0300

    update the project's folder with the express-api-template

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,20 +1,20 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'personal-and-professional-db'
+const mongooseBaseName = 'personal-and-professional-page'
 
 // create the mongodb uri for development and test
 const database = {
-    development: `mongodb://localhost/${mongooseBaseName}-development`,
-    test: `mongodb://localhost/${mongooseBaseName}-test`
+  development: `mongodb://localhost/${mongooseBaseName}-development`,
+  test: `mongodb://localhost/${mongooseBaseName}-test`
 }
 
 // Identify if development environment is test or development
 // select DB based on whether a test file was executed before `server.js`
-const localDB = process.env.TESTENV ? database.test : database.development
+const localDb = process.env.TESTENV ? database.test : database.development
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDB = process.env.MONGODB_URI || localDB
+const currentDb = process.env.MONGODB_URI || localDb
 
-module.exports = currentDB
+module.exports = currentDb
--- Server/server.js
+++ Server/server.js
@@ -1,54 +1,80 @@
-//Require nesse NPM Packege
+// require necessary NPM packages
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors=require('cors')
-//import Router
-const indexRouter = require("./app/routes/index");
-const UserRouter = require("./app/routes/users");
-//Require DB
+// require route files
+const exampleRoutes = require('./app/routes/example_routes')
+const userRoutes = require('./app/routes/user_routes')
 
-const db = require("./config/db");
+// require error handling middleware
+const errorHandler = require('./lib/error_handler')
 
-//Establish DB connection
+// require database configuration logic
+// `db` will be the actual Mongo URI as a string
+const db = require('./config/db')
 
-mongoose.connect(db, { useNewParser: true });
-mongoose.connection.once("open", () => {
-  console.log("Connected to mongoose");
-});
+// require configured passport authentication middleware
+const auth = require('./lib/auth')
 
-const app = express();
+// required middleware to log requests
+const requestLogger = require('./lib/request_logger')
 
-//Define the port for the api
+// require middleware for accepting token or bearer
+const tokenOrBearer = require('./lib/token_or_bearer')
 
-const port = process.env.Port || 5000;
-const reactPort =3000
+// Define Ports
+const reactPort = 3000
+const expressPort = 5000
 
+// establish database connection
+mongoose.Promise = global.Promise
+mongoose.connect(db,
+  { useNewUrlParser: true }
+)
 
+// instantiate express application object
+const app = express()
 
-//app.use(expres.json());
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`}))
 
-//SetCors
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` }))
+// define port for API to run on
+const port = process.env.PORT || expressPort
 
-//** MiddleWear*/
-//Add `body Prser `
+// this middleware makes it so the client can use the Rails convention
+// of `Authorization: Token <token>` OR the Express convention of
+// `Authorization: Bearer <token>`
+app.use(tokenOrBearer)
 
-app.use(express.json());
+// register passport authentication middleware
+app.use(auth)
 
-/** Mount omported Router
- *
- *
- *
- */
-app.use(indexRouter);
-app.use(UserRouterRouter);
-//Start the ser
+// add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+// The method `.use` sets up middleware for the Express application
+app.use(bodyParser.json())
+// this parses requests sent by `$.ajax`, which use a different content type
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// add request logger to create server log
+app.use(requestLogger)
+
+// register route files
+app.use(exampleRoutes)
+app.use(userRoutes)
+
+// register error handling middleware
+// note that this comes after the route middlewares, because it needs to be
+// passed any error messages from them
+app.use(errorHandler)
+
+// run API on designated port (3000 in this case)
 app.listen(port, () => {
-  console.log(`Blogy listen${port}`);
-});
+  console.log('listening on port' + port)
+})
 
-/**
- 
- */
+// needed for testing
+module.exports = app
